54213884597bd7cea908939dfaae033ac1bf38c0
BezzB <118984781+BezzB@users.noreply.github.com>
2023-05-15 17:30:19 +0300

    Update todoitem

--- react-todo-start/components/TodoItem.jsx
+++ react-todo-start/components/TodoItem.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 const TodoItem = ({ itemProp }) => {
     return (
       <li>
+        // eslint-disable-next-line react/jsx-no-comment-textnodes
         <input type="checkbox" />
+        // eslint-disable-next-line react/prop-types
         {itemProp.title}
       </li>
     );
